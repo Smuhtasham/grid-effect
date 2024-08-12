8dd54770ee070cb410c20e3173d1b2279cd4fb9e
Syed Muhammad Muhtasham <muhtasham00@gmail.com>
2024-08-12 15:09:46 +0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./input.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div class="container">
+        <div class="item" data-order="1"></div>
+        <div class="item" data-order="2"></div>
+        <div class="item" data-order="3"></div>
+        <div class="item" data-order="4"></div>
+        <div class="item" data-order="5"></div>
+        <div class="item" data-order="6"></div>
+        <div class="item" data-order="7"></div>
+      </div>
+    </>
   );
 }
 
